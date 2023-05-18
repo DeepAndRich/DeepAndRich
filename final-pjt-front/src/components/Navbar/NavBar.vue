89ce4bbddcdddba0869ce8c03d690ec2cea729af
BTDnoBacon <osabero@naver.com>
2023-05-18 17:05:45 +0900

    NAVBAR 완성 NAVBAR CSS 및, Component 구성

---
+++ final-pjt-front/src/components/Navbar/NavBar.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="py-11 px-14 w-full h-36 mx-auto fixed">
+		<nav
+			class="text-xl flex box-content p-0 flex-wrap items-center justify-between"
+		>
+			<div class="flex items-center">
+				<router-link to="/" class="w-80 mr-2"
+					><img class="w-80" src="@/assets/img/logo.png" alt=""
+				/></router-link>
+				<div v-for="item in navList" :key="item.title">
+					<NavBarLink :item="item" />
+				</div>
+			</div>
+			<div class="flex items-center">
+				<button class="mx-2 font-bold">로그인</button>
+				<button class="mx-2 w-40 h-10 font-bold bg-themeyellow rounded-lg">
+					회원가입
+				</button>
+			</div>
+		</nav>
+	</div>
+</template>
+
+<script>
+import NavBarLink from '@/components/Navbar/NavBarLink.vue';
+export default {
+	name: 'NavBar',
+	components: {
+		NavBarLink,
+	},
+	data() {
+		return {
+			navList: [
+				{
+					URL: '/finance',
+					title: '예 적금 비교',
+				},
+				{
+					URL: '/algorithm',
+					title: '추천 알고리즘',
+				},
+				{
+					URL: '/community',
+					title: '커뮤니티',
+				},
+				{
+					URL: '/bank',
+					title: '은행',
+				},
+				{
+					URL: '/exchange',
+					title: '환율',
+				},
+			],
+		};
+	},
+};
+</script>
+
+<style></style>
