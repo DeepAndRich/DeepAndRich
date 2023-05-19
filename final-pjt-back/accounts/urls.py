5b275543f378f804a34e1d7dd0e5a4c4a89422a6
hyejinKim <zlagg23@gmail.com>
2023-05-19 17:51:04 +0900

    articles 이미지 해야함

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test),
+    # path('test/', views.test),
+    # path('accounts/update', views.update, )
 ]
