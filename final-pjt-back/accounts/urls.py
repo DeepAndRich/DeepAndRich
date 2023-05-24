5e83cc356ce5a9ca762d43a95cfa425e1c11566a
hyejinKim <zlagg23@gmail.com>
2023-05-24 17:58:49 +0900

    가입상품조회

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     # 저장 / 삭제
-    path('save_myproduct/<int:product_pk>/', views.save_myproduct),
+    path('save-deposit-myproduct/<int:product_pk>/', views.save_deposit_myproduct),
+    path('save-saving-myproduct/<int:product_pk>/', views.save_saving_myproduct),
     # 조회
     path('<int:user_pk>/myproduct/', views.myproduct),
+    # path('<int:user_pk>/saving-myproduct/', views.saving_myproduct),
+    # path('<int:user_pk>/user_img/', views.user_img),
 ]
+# + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
