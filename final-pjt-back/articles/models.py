1fdc015fe72895f3e4841de060efadc08e2e676c
hyejinKim <zlagg23@gmail.com>
2023-05-24 14:53:48 +0900

    게시글 상세조회시 작성자 이름 반환

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -7,13 +7,14 @@ from django.conf import settings
 
 # Create your models here.
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author = models.CharField(max_length=20, blank=True)
     title = models.CharField(max_length=10)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(null=True)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles', blank=True)
+    
 
 
 class Comment(models.Model):
