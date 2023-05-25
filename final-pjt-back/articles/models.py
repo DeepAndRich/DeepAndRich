ba099672bdd6399f38f658cb3416f9a8a0cd1e96
hyejinki <zlagg23@gmail.com>
2023-05-25 22:02:47 +0900

    dd

--- final-pjt-back/articles/models.py
+++ final-pjt-back/articles/models.py
@@ -22,3 +22,4 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=20, blank=True)
